bd1297b23eb5790e5be94919bb17e99bfe0bba6b
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2024-03-10 02:55:05 -0300

    feat: finished app that generates luck numbers

---
+++ SortNumberApp/SortNumberApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using SortNumberApp.Views;
+
+namespace SortNumberApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}

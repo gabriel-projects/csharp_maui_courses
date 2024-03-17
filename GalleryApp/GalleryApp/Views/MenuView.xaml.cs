d459defe9e13bc23c8e504e5430800754b0a3b1b
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2024-03-17 18:55:13 -0300

    feat: creating navigation in menu view

--- GalleryApp/GalleryApp/Views/MenuView.xaml.cs
+++ GalleryApp/GalleryApp/Views/MenuView.xaml.cs
@@ -1,4 +1,5 @@
 using GalleryApp.Repositories;
+using Microsoft.Maui.Controls;
 
 namespace GalleryApp.Views;
 
@@ -14,20 +15,40 @@ public partial class MenuView : ContentPage
 		{
 			var lblCategory = new Label();
 			lblCategory.Text = category.Name;
+			lblCategory.FontFamily = "OpenSansSemibold";
 
             MenuContainer.Children.Add(lblCategory);
 
             foreach (var component in category.Components)
 			{
+				var tap = new TapGestureRecognizer();
+				tap.CommandParameter = component.Page;
+                tap.Tapped += OnTapComponent;
+
 				var lblComponentTitle = new Label();
 				lblComponentTitle.Text = component.Title;
+				lblComponentTitle.FontFamily = "OpenSansSemibold";
+				lblComponentTitle.Margin = new Thickness(20, 20, 0, 0);
+				lblComponentTitle.GestureRecognizers.Add(tap);
 
-				var lblComponentDescription = new Label();
+                var lblComponentDescription = new Label();
 				lblComponentDescription.Text = component.Description;
+				lblComponentDescription.Margin = new Thickness(20, 0, 0, 0);
+				lblComponentDescription.GestureRecognizers.Add(tap);
 
                 MenuContainer.Children.Add(lblComponentTitle);
                 MenuContainer.Children.Add(lblComponentDescription);
             }
 		}
 	}
+
+    private void OnTapComponent(object sender, TappedEventArgs e)
+    {
+		var label = (Label)sender;
+		var tap = (TapGestureRecognizer)label.GestureRecognizers[0];
+		var page = (Page)tap.CommandParameter;
+
+		((FlyoutPage)App.Current.MainPage).Detail = page;
+		((FlyoutPage)App.Current.MainPage).IsPresented = false;
+    }
 }
